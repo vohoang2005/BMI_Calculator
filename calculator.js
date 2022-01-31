8a1046b883464cbc92342d4b9e84f5c0cf110d74
vohoang2005 <vohoang2005@gmail.com>
2022-01-31 11:03:53 -0800

    updated

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res) {
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of the addition calculation is " + result);
+});
+
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI result is " + bmi);
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000")
+});
